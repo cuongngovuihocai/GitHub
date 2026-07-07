1cd24154e55797ae386172a9a4fa4120e00b5ab0
cuongngovuihocai <cuongngovuihocai@gmail.com>
2026-07-07 14:08:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    // 👇 BẮT BUỘC: Thêm dòng này
+    base: '/GitHub/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
